93fcc1f9eaaa860b484d737d81dbd3ccaef5937c
QuadeGarner <quadegar@gmail.com>
2025-04-19 02:25:16 -0500

    added controller class

---
+++ src/Controller/PanelController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+public class PanelController {
+    // This class is responsible for controlling the panels in the application
+    // It will handle the interactions between the FieldPanel and SidebarPanel
+
+    public PanelController() {
+        // Constructor for PanelController
+    }
+
+    public void updateFieldPanel() {
+        // Method to update the FieldPanel
+    }
+
+    public void updateSidebarPanel() {
+        // Method to update the SidebarPanel
+    }
+    
+}
